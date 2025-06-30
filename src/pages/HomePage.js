98e4f937134d77e6c4aa8fa45ae48c8e3e97a793
Rein van der Vegt <ren.vegt@gmail.com>
2025-06-30 16:45:33 +0200

    add exercise grid images

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,7 @@ function HomePage() {
   return (
     <Container fluid={true}>
       <h2 className='titel'>Home</h2>
+      <span style={{backgroundColor:'#ffc107'}}>Home</span>
     </Container>    
   );
 }
--- src/pages/UpperBodyPage.js
+++ src/pages/UpperBodyPage.js
@@ -2,49 +2,55 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Stack from 'react-bootstrap/Stack';
 
 function UpperBodyPage() {
   return (
     <>
       <Container fluid={true}>
-        <h2 className='titel'>UpperBody</h2>
-        <Row>
-          <Col className="exersise-header">
-            Exersise
+        <h2 className='titel'>UpperBody exercises</h2>
+        <Row className="exercise-grid">
+          <Col sm>
+            <h3>Arm curl</h3>
           </Col>
-          <Col className="exersise-header">
-            How to
+          <Col sm>
+            <h3>Push up</h3>
           </Col>
-        </Row>
-
-      <Stack direction="vertical"gap={10}>
-         <Row>
-          <Col>
-           
-            <ul className="exersise-list">
-              <li>Back</li> 
-              <li>Chest</li>
-              <li>Triceps</li>
-              <li>Biceps</li>
-              <li>Ahoulders</li>
-              <li>Abs</li>
-            </ul>
+          <Col sm>
+            <h3>Chest press</h3>
           </Col>
-        
-          <Col>
-          
-            <ul className="exersise-list">
-              <li>First item</li>
-              <li>Second item</li>
-              <li>Third item</li>
-            </ul>
+          <Col sm>
+            <h3>Incline press</h3>
           </Col>
         </Row>
-      </Stack> 
+        <Row className="exercise-grid">
+          <Col sm>
+            <h3>Arm curl</h3>
+          </Col>
+          <Col sm>
+            <h3>Push up</h3>
+          </Col>
+          <Col sm>
+            <h3>Chest press</h3>
+          </Col>
+          <Col sm>
+            <h3>Incline press</h3>
+          </Col>
+        </Row>
+        <Row className="exercise-grid">
+          <Col sm>
+            <h3>Arm curl</h3>
+          </Col>
+          <Col sm>
+            <h3>Push up</h3>
+          </Col>
+          <Col sm>
+            <h3>Chest press</h3>
+          </Col>
+          <Col sm>
+            <h3>Incline press</h3>
+          </Col>
+        </Row>
      </Container>    
-         
-      
     </>
   );
 }
