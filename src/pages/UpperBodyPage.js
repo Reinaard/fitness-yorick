8952ba7e02850793e91518c5884e22bd06b8ce01
Rein van der Vegt <ren.vegt@gmail.com>
2025-07-02 15:22:49 +0200

    hallo

--- src/pages/UpperBodyPage.js
+++ src/pages/UpperBodyPage.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function UpperBodyPage() {
   const [users, setUsers] = useState([]);
+  const [aantalToegevoegd, setAantalToegevoegd] = useState([]);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -20,124 +21,57 @@ function UpperBodyPage() {
     fetchUsers();
   }, []);
 
+
+    function voegToeAanLijst(exercise) {
+      const element = document.getElementById(`tegel-${exercise.id}`);
+      document.styleSheets[0].insertRule(`tegel-${exercise.id}::before { content: 'Welcome! '; }`, 0);
+      if (!aantalToegevoegd.includes(exercise.name)){
+        setAantalToegevoegd([...aantalToegevoegd, exercise.name])
+        element.style.color = 'green'
+      }
+      else {
+        const updatedArray = [...aantalToegevoegd].filter(item => item !== exercise.name);
+        setAantalToegevoegd(updatedArray)
+        element.style.color = 'black'
+      }
+    }
+
+    function oefeningenLatenZien(latenZien) {
+      document.getElementById("teller").style.display = latenZien ? "block" : "none"
+      // display
+    }
   return (
     <>
       <Container fluid={true}>
         <h2 className='titel'>UpperBody exercises</h2>
         <Row className="exercise-grid">
           {users.map((exercise) => (
-          <Col sm className="btn-container">
-            <h3>{exercise.name}</h3>
-            <span>{exercise.category}</span>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-            ))}
-          {/* <Col sm className="btn-container">
-            <h3>Lat Pulldown</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-          <Col sm className="btn-container">
-            <h3>Lat Row</h3>
-             <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-          <Col sm className="btn-container">
-            <h3>Lat Pullover</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-          <Col sm className="btn-container">
-            <h3>Preacher Curl</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
+            <Col sm className="btn-container">
+              <h3>{exercise.name}</h3>
+              <span>{exercise.category}</span>
+              <div className='button'>
+              <div className='button-plus' onClick={()=>{voegToeAanLijst(exercise)}} id={`tegel-${exercise.id}`}></div>
+              </div>
+
+            </Col>
+          ))}
         </Row>
-        <Row className="exercise-grid">
-          <Col sm className="btn-container">
-            <h3>Bicep Curl</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-          <Col sm className="btn-container">
-            <h3>Hammer Curl</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-          <Col sm className="btn-container">
-            <h3>Tricep Press</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-          <Col sm className="btn-container">
-            <h3>Tricep Pushdown</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-        </Row>
-        <Row className="exercise-grid">
-          <Col sm className="btn-container">
-            <h3>Lateral Raises</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-          <Col sm className="btn-container">
-            <h3>Shoudler Press</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-          <Col sm className="btn-container">
-            <h3>Chest press</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-          <Col sm className="btn-container">
-            <h3>Incline press</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-        </Row>
-        <Row className="exercise-grid">
-          <Col sm className="btn-container">
-            <h3>Chest Fly</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-          <Col sm className="btn-container">
-            <h3>Abs Upper</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-          <Col sm className="btn-container">
-            <h3>Abs Lower</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col>
-          <Col sm className="btn-container">
-            <h3>Fore Arms</h3>
-            <div className='button' onclick="alert('you done clicked the button')">
-              <div className='button-plus'></div>
-            </div>
-          </Col> */}
-        </Row>
-     </Container>    
+      </Container>
+      <div id="teller">
+        <ul>
+          {aantalToegevoegd.map((exercise) => (
+            <li>{exercise}</li>
+          ))}
+        </ul>
+      </div>    
+      <button
+        id="ovalButton"
+        onClick={()=>{alert(`${aantalToegevoegd.length} oefeningen geselecteerd!`)}}
+        onMouseOver={()=>{oefeningenLatenZien(true)}}
+        onMouseOut={()=>{oefeningenLatenZien(false)}}
+      >
+        {aantalToegevoegd.length}
+      </button>
     </>
   );
 }
